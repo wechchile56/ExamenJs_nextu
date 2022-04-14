2706ea8a272ec1750e0249bdf0c3935d16e5b1a6
wechchile <wechcandes@gmail.com>
2022-04-14 11:36:09 -0400

    se creand dos procedimiento para el programa de juego

---
+++ js/app.js
@@ -0,0 +1,22 @@
+// ****************************************
+// juego con js
+// creado wehchchile
+// fecha 14 de abril 2022
+// tiene con fin de tener todos los procedimiento necesario
+// para ejecutar el programa
+// ************************************
+
+
+
+// function hacer el random de las imagen del juego;
+// funcione cuenta con un arreglo donde se encuentra cargado los ubicacion
+// de cada imagen
+function randompic(){
+	var arreglo = new Array("image/1.png","image/2.png","image/3.png","image/4.png", );
+	var Resultado =  Math.floor(Math.random()* arrelgo.lengths);
+}
+
+// function hacer llenado cuando inicia el programa.
+function llenado(){
+
+}
