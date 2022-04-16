8937e6d90fb3246925a543e579a2e9ae76cbfac3
wechchile <wechcandes@gmail.com>
2022-04-15 20:10:56 -0400

    control de titulo

--- js/app.js
+++ js/app.js
@@ -6,17 +6,33 @@
 // para ejecutar el programa
 // ************************************
 
-
+var $Juego = new Array[6][6];
 
 // function hacer el random de las imagen del juego;
 // funcione cuenta con un arreglo donde se encuentra cargado los ubicacion
 // de cada imagen
+
 function randompic(){
 	var arreglo = new Array("image/1.png","image/2.png","image/3.png","image/4.png", );
-	var Resultado =  Math.floor(Math.random()* arrelgo.lengths);
+	 result =  Math.floor(Math.random()* arrelgo.lengths);
 }
 
 // function hacer llenado cuando inicia el programa.
 function llenado(){
+	for (var i =0; i < 7; i++) {
+		for (var ii =0; i < 7; i++) {
+		
+	}		
+	}
 
+}
+
+// animacion de titulo
+
+function AnimacionTitulo(){
+	$('main-titulo').animate({
+		opacity:'1';
+		color: yellow;
+
+	},3000)
 }
